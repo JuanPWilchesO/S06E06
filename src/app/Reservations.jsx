67c40aefb7ad310cecbbcc8bf77ae9da7c3cbc1c
JuanPabloWO <juanpablowo@gmail.com>
2024-12-08 12:45:18 -0500

    first_commit

---
+++ src/app/Reservations.jsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from "react"
+import { Bounce, toast, ToastContainer } from 'react-toastify'
+import useApiFetch from "../hooks/useApiFetch"
+import ReservationsList from "../components/reservations/ReservationsList"
+import Modal from "../components/Modal"
+import Review from "../components/reservations/Review"
+
+function Reservations() {
+  const [reservations, fetchReservations, error, loading] = useApiFetch()
+  const [openModal, setOpenModal] = useState(false)
+  const [child, setChild] = useState(null)
+  useEffect(() => {
+    fetchReservations({
+      url: '/bookings'
+    })
+  }, [])
+
+  const handleDelete = (id) => {
+    fetchReservations({
+      url: `/bookings/${id}`,
+      method: 'DELETE'
+    })
+    toast.info('Reservation deleted!', {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+      transition: Bounce,
+    })
+  }
+
+  const closeModal = () => {
+    setOpenModal(false)
+  }
+
+  const handleOpenModal = (id) => {
+    setOpenModal(true)
+    setChild(<Review hotelId={id} closeModal={closeModal}/>)
+  }
+  
+  return (
+    <div className="max-w-5xl mx-auto px-5 py-16">
+      <ToastContainer/>
+      <ReservationsList
+      reservations={reservations}
+      onDelete={handleDelete}
+      onRate={handleOpenModal}
+      />
+
+      <Modal
+      openModal={openModal}
+      closeModal={closeModal}
+      >
+        {child}
+      </Modal>
+    </div>
+  )
+}
+
+export { Reservations }
